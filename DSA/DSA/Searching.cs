39738a9df2676da43cc66813703bce8cf33474ae
Soserocky <sabya9106@gmail.com>
2022-09-13 23:48:44 +0530

    Push chages into Git

--- DSA/DSA/Searching.cs
+++ DSA/DSA/Searching.cs
@@ -14,7 +14,11 @@ namespace DSA
             //Console.WriteLine(BinarySearch_Recursive(new int[7] { 1, 2, 3, 4, 5, 6, 7 }, 6, 0, 6));
             //Console.WriteLine(IndexOfFirstOccurenceOfElementInSortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 5));
             //Console.WriteLine(IndexOfLastOccurenceOfElementInSortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 5));
-            Console.WriteLine(CountOccurenceOfNumberInSortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 5));
+            //Console.WriteLine(CountOccurenceOfNumberInSortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 5));
+            //Console.WriteLine(SearchElementInInfinitelySortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 5));
+            //Console.WriteLine(FindPeakElementInNonSortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }));
+            //FindPairOfElementsWithAGivenSumInASortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 10);
+            FindTripletsOfElementsWithAGivenSumInASortedArray(new int[8] { 1, 2, 3, 4, 5, 5, 6, 7 }, 10);
             //Console.WriteLine(SquareRoot(1000));
         }
 
@@ -188,6 +192,75 @@ namespace DSA
                 else break;
             }
             return count;
+        }
+
+        internal static int SearchElementInInfinitelySortedArray(int[] input, int number)
+        {
+            //In an infinite array, you can not do mid = (low + high) / 2
+            //as high = input.length - 1, a very high number.
+
+            if (input[0] == number) return 0;
+
+            int index = 1;
+            while (input[index] < number)
+                index *= 2;
+            
+            if (input[index] == number) return index;
+
+            var destinationArray = new int[index /2 + 1];
+            Array.Copy(input, index / 2, destinationArray, 0, index / 2 + 1);
+
+            return BinarySearch_Iterative(destinationArray, number);
+        }
+
+        internal static int FindPeakElementInNonSortedArray(int[] input)
+        {
+            int low = 0, high = input.Length - 1;
+            while (low <= high)
+            {
+                if (high - low == 1) 
+                {
+                    if (input[low] >= input[high]) return input[low];
+                    else return input[high];
+                }
+                var mid = (low + high) / 2;
+                if (input[mid] >= input[mid - 1] && input[mid] >= input[mid + 1]) return input[mid];
+                else if (input[mid - 1] >= input[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            return -1;
+        }
+
+        internal static void FindPairOfElementsWithAGivenSumInASortedArray(int[] input, int number)
+        {
+            int low = 0, high = input.Length - 1;
+            while (low < high)
+            {
+                var sum = input[low] + input[high];
+                if (sum == number)
+                {
+                    Console.WriteLine($"Pair is: ${input[low]}, {input[high]}");
+                    return;
+                }
+                else if (sum < number) high--;
+                else low++;
+            }
+        }
+
+        internal static void FindTripletsOfElementsWithAGivenSumInASortedArray(int[] input, int number)
+        {
+            int low = 0, high = input.Length - 1;
+            while (low < high)
+            {
+                var sum = input[low] + input[high];
+                if (sum == number)
+                {
+                    Console.WriteLine($"Pair is: ${input[low]}, {input[high]}");
+                    return;
+                }
+                else if (sum < number) high--;
+                else low++;
+            }
         }
         internal static int SquareRoot(int number)
         {
