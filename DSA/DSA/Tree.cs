2e4579e9c7bf6fde1a4f02e33b03e591dc456624
Soserocky <sabya9106@gmail.com>
2022-11-02 00:47:16 +0530

    Pushing changes into Git

--- DSA/DSA/Tree.cs
+++ DSA/DSA/Tree.cs
@@ -7,7 +7,17 @@
             var root = ConstructTree();
             //InOrderTraversal(root);
             //PreOrderTraversal(root);
-            PostOrderTraversal(root);
+            //PostOrderTraversal(root);
+            //HeightOfBinaryTree(root);
+            //PrintNodesAtDistanceKFromRoot(root, 2, 0);
+            //LevelOrderTraversal_Naive(root);
+            //var queue = new Queue<TreeNode>();
+            //queue.Enqueue(root);
+            //LevelOrderTraversal_Efficient(queue);
+            //SizeOfBinaryTree(root);
+            //MaximumOfBinaryTree(root);
+            //PrintLeftViewOfBinaryTree(root);
+            //ChildrenSumProperty(root);
         }
 
         private static TreeNode ConstructTree()
@@ -41,7 +51,6 @@
 
             return root;
         }
-
         private static void InOrderTraversal(TreeNode root)
         {
             if (root != null)
@@ -64,11 +73,148 @@
         {
             if (root != null)
             {
+                Console.Write(root.Value + "  ");
                 PostOrderTraversal(root.Left);
                 PostOrderTraversal(root.Right);
-                Console.Write(root.Value + "  ");
             }
         }
+        private static int HeightOfBinaryTree(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var h1 = HeightOfBinaryTree(root.Left);
+            var h2 = HeightOfBinaryTree(root.Right);
+
+            if (h1 > h2) return h1 + 1;
+
+            else return h2 + 1;
+        }
+        private static void PrintNodesAtDistanceKFromRoot(TreeNode root, int k, int distance)
+        {
+            if (root == null) return;
+
+            if (k == distance)
+            {
+                Console.Write($"{root.Value} ");
+                return;
+            }
+
+            PrintNodesAtDistanceKFromRoot(root.Left, k, distance + 1);
+            PrintNodesAtDistanceKFromRoot(root.Right, k, distance + 1);
+        }
+        private static void LevelOrderTraversal_Naive(TreeNode root)
+        {
+            var height = HeightOfBinaryTree(root);
+            for (int i = 0; i <= height; i++)
+            {
+                PrintNodesAtDistanceKFromRoot(root, i, 0);
+                Console.WriteLine("");
+            }
+        }
+        private static void LevelOrderTraversal_Efficient(Queue<TreeNode> queue)
+        {
+            var queue2 = new Queue<TreeNode>();
+            while (queue.Count() > 0)
+            {
+                var node = queue.Dequeue();
+                Console.Write(node.Value + " ");
+                if (node.Left != null) queue2.Enqueue(node.Left);
+                if (node.Right != null) queue2.Enqueue(node.Right);
+            }
+            if (queue2.Count() > 0)
+            {
+                Console.WriteLine("");
+                LevelOrderTraversal_Efficient(queue2);
+            }
+        }
+        private static int SizeOfBinaryTree(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            return 1 + SizeOfBinaryTree(root.Left) + SizeOfBinaryTree(root.Right);
+        }
+        private static int MaximumOfBinaryTree(TreeNode root)
+        {
+            if (root == null) return int.MinValue;
+
+            var leftMax = MaximumOfBinaryTree(root.Left);
+            var rightMax = MaximumOfBinaryTree(root.Right);
+            var current = root.Value;
+
+            if (current >= leftMax && current >= rightMax) return current;
+
+            else if (leftMax >= current && leftMax >= rightMax) return leftMax;
+
+            return rightMax;
+        }
+        private static void PrintLeftViewOfBinaryTree(TreeNode root)
+        {
+           if (root == null) return;
+
+           var queue = new Queue<TreeNode>();
+           queue.Enqueue(root);
+           PrintLeftViewOfBinaryTree_Helper(queue);
+        }
+        private static void PrintLeftViewOfBinaryTree_Helper(Queue<TreeNode> queue)
+        {
+            var queue2 = new Queue<TreeNode>();
+            var count = 0;
+            while (queue.Count() > 0)
+            {
+                var node = queue.Dequeue();
+                if (count == 0)
+                {
+                    Console.WriteLine(node.Value + " ");
+                    count++;
+                }
+                if (node.Left != null) queue2.Enqueue(node.Left);
+                if (node.Right != null) queue2.Enqueue(node.Right);
+            }
+            if (queue2.Count() > 0) PrintLeftViewOfBinaryTree_Helper(queue2);
+        }
+        private static bool ChildrenSumProperty(TreeNode root)
+        {
+            if (root == null) return true;
+
+            var left  = root.Left;
+            var right = root.Right;
+
+            if (left == null && right == null) return true;
+
+            var leftValue = left == null ? 0 : left.Value;
+            var rightValue = right == null ? 0 : right.Value;
+
+            if (root.Value == leftValue + rightValue) return ChildrenSumProperty(left) && ChildrenSumProperty(right);
+
+            return false;
+        }
+        private static int MaxWidthOfBinaryTree(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            return MaxWidthOfBinaryTree_Helper(queue, 1);
+        }
+        private static int MaxWidthOfBinaryTree_Helper(Queue<TreeNode> queue, int maxWidth)
+        {
+            var queue2 = new Queue<TreeNode>();
+            var count = 0;
+            while (queue.Count() > 0)
+            {
+                var node = queue.Dequeue();
+                count++;
+
+                if (node.Left != null) queue2.Enqueue(node.Left);
+                if (node.Right != null) queue2.Enqueue(node.Right);
+            }
+            if (count > maxWidth) maxWidth = count;
+            if (queue2.Count() > 0) return MaxWidthOfBinaryTree_Helper(queue2, maxWidth);
+
+            return maxWidth;
+        }
+
     }
 
     internal class TreeNode
